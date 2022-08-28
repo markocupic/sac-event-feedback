dcbc3b32de5c38d5916b3f7f2d7c9bd876608b14
Marko Cupic <m.cupic@gmx.ch>
2022-08-28 23:09:52 +0200

    Fix undefinded array key issues

--- src/Resources/contao/dca/tl_event_feedback.php
+++ src/Resources/contao/dca/tl_event_feedback.php
@@ -77,7 +77,7 @@ $GLOBALS['TL_DCA']['tl_event_feedback'] = [
                 'label'      => &$GLOBALS['TL_LANG']['tl_event_feedback']['delete'],
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
             'show'   => [
                 'label'      => &$GLOBALS['TL_LANG']['tl_event_feedback']['show'],
--- src/Resources/contao/dca/tl_event_feedback_reminder.php
+++ src/Resources/contao/dca/tl_event_feedback_reminder.php
@@ -63,7 +63,7 @@ $GLOBALS['TL_DCA']['tl_event_feedback_reminder'] = [
             'delete' => [
                 'href'       => 'act=delete',
                 'icon'       => 'delete.gif',
-                'attributes' => 'onclick="if(!confirm(\''.$GLOBALS['TL_LANG']['MSC']['deleteConfirm'].'\'))return false;Backend.getScrollOffset()"',
+                'attributes' => 'onclick="if(!confirm(\''.($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null).'\'))return false;Backend.getScrollOffset()"',
             ],
 
             'show' => [
