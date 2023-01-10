a9f3880db9de6d0ee4320d92377219f19bb5c7a3
Marko Cupic <m.cupic@gmx.ch>
2023-01-10 22:37:59 +0100

    Add backend assets via Event Subscriber

---
+++ src/EventSubscriber/BackendAssetsSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SAC Event Feedback.
+ *
+ * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/sac-event-feedback
+ */
+
+namespace Markocupic\SacEventFeedback\EventSubscriber;
+
+use Contao\CoreBundle\Routing\ScopeMatcher;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class BackendAssetsSubscriber implements EventSubscriberInterface
+{
+    protected ScopeMatcher $scopeMatcher;
+
+    public function __construct(ScopeMatcher $scopeMatcher)
+    {
+        $this->scopeMatcher = $scopeMatcher;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [KernelEvents::REQUEST => 'registerBackendAssets'];
+    }
+
+    public function registerBackendAssets(RequestEvent $e): void
+    {
+        $request = $e->getRequest();
+
+        if ($this->scopeMatcher->isBackendRequest($request)) {
+            // Add Backend CSS
+            $GLOBALS['TL_CSS'][] = 'bundles/markocupicsaceventfeedback/css/styles.css|static';
+        }
+    }
+}
