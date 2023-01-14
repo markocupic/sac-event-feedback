970eb9711083806ce34c05546b7ead1bd828b8df
Marko Cupic <marko.cupic@schule-ettiswil.ch>
2023-01-14 15:19:45 +0100

    Add missing translation strings

---
+++ contao/languages/en/tl_calendar_events_member.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of SAC Event Feedback.
+ *
+ * (c) Marko Cupic 2023 <m.cupic@gmx.ch>
+ * @license MIT
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/markocupic/sac-event-feedback
+ */
+
+// Legends
+$GLOBALS['TL_LANG']['tl_calendar_events_member']['onlineFeedback_legend'] = 'Einstellungen für die Event Auswertung';
+
+// Fields
+$GLOBALS['TL_LANG']['tl_calendar_events_member']['countOnlineEventFeedbackNotifications'] = ['Bereits versendete Reminder', 'Hier sehen Sie die Anzahl bereits versendeter Benachrichtigungen, in denen der Teilnehmer angehalten wurde, das Formular für die Event-Auswertung auszufüllen.'];
